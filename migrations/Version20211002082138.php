bb30d537588aa820c8692710cda504a64c3a341f
jojogames632 <jojogames@free.fr>
2021-10-02 10:25:44 +0200

    Create entities and database + set host link in .env file

---
+++ migrations/Version20211002082138.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211002082138 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, holder_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, picture_filename VARCHAR(255) NOT NULL, release_date DATE NOT NULL, description LONGTEXT NOT NULL, author VARCHAR(255) NOT NULL, genre VARCHAR(255) NOT NULL, reservation_date DATETIME DEFAULT NULL, is_borrowed TINYINT(1) NOT NULL, borrowing_date DATETIME DEFAULT NULL, INDEX IDX_CBE5A331DEEE62D0 (holder_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A331DEEE62D0 FOREIGN KEY (holder_id) REFERENCES `user` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book DROP FOREIGN KEY FK_CBE5A331DEEE62D0');
+        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pictureFilename;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $releaseDate;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $genre;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $reservationDate;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isBorrowed;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $borrowingDate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="books")
+     */
+    private $holder;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPictureFilename(): ?string
+    {
+        return $this->pictureFilename;
+    }
+
+    public function setPictureFilename(string $pictureFilename): self
+    {
+        $this->pictureFilename = $pictureFilename;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate(\DateTimeInterface $releaseDate): self
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getReservationDate(): ?\DateTimeInterface
+    {
+        return $this->reservationDate;
+    }
+
+    public function setReservationDate(?\DateTimeInterface $reservationDate): self
+    {
+        $this->reservationDate = $reservationDate;
+
+        return $this;
+    }
+
+    public function getIsBorrowed(): ?bool
+    {
+        return $this->isBorrowed;
+    }
+
+    public function setIsBorrowed(bool $isBorrowed): self
+    {
+        $this->isBorrowed = $isBorrowed;
+
+        return $this;
+    }
+
+    public function getBorrowingDate(): ?\DateTimeInterface
+    {
+        return $this->borrowingDate;
+    }
+
+    public function setBorrowingDate(?\DateTimeInterface $borrowingDate): self
+    {
+        $this->borrowingDate = $borrowingDate;
+
+        return $this;
+    }
+
+    public function getHolder(): ?User
+    {
+        return $this->holder;
+    }
+
+    public function setHolder(?User $holder): self
+    {
+        $this->holder = $holder;
+
+        return $this;
+    }
+}
