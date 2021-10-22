7d5f6899d28a6fdd796cc43690bb8f6c03f5f410
jojogames632 <jojogames@free.fr>
2021-10-22 14:57:40 +0200

    You need now to login after /register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('registrant_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
