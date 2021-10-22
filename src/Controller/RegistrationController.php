ef546f920ebc79d1f7d4a5c1762c905c68b2c969
jojogames632 <jojogames@free.fr>
2021-10-22 14:27:25 +0200

    New registrant accounts are now auto validate for testing application

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -29,7 +29,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-            $user->setIsActive(false);
+            $user->setIsActive(true);
             $user->setRoles(["ROLE_REGISTRANT"]);
 
             $entityManager = $this->getDoctrine()->getManager();
